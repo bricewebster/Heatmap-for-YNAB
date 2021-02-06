c3eb04a46a98a1ca2e9d151566d7ced40f025baf
Brice Webster <erodedpretzel@gmail.com>
2021-02-06 10:55:05 -0500

    Rough draft of yearly calendar and established basic connection with ynab

---
+++ js/test.js
@@ -0,0 +1,14 @@
+var ynab = window.ynab;
+var accessToken = persAuthToken.data;
+
+//alert(accessToken);
+
+const ynabAPI = new ynab.API(accessToken);
+
+(async function() {
+  const budgetsResponse = await ynabAPI.budgets.getBudgets();
+  const budgets = budgetsResponse.data.budgets;
+  for(let budget of budgets) {
+    console.log(`Budget Name: ${budget.name}`);
+  }
+})();
