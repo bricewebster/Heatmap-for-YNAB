a17d2a67a54d04a9a74998dc6102b903ef08c3cc
bwebster <erodedpretzel@gmail.com>
2021-02-08 15:47:44 -0500

    Put accessToken and Budget ID into async functions for better consistency. Fetching transactions put into async function so that there is only the 1 fetch when the page first loads.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-getTransactions();
+main()
 
-function getTransactions () {
+async function main() {
+  const accessToken = await getAccessToken();
+  const mainBudgetID = await getBudgetID();
+  const transactions = await getTransactions(accessToken, mainBudgetID);
+  console.log(transactions);
+  //storeTransactions();
+}  
+
+async function getAccessToken() {
+  const response = await fetch('.vscode/accessToken.txt');
+  const accessToken = await response.text();
+  return accessToken;
+}
+
+async function getBudgetID() {
+  const response = await fetch('.vscode/budgetID.txt');
+  const budgetID = await response.text();
+  return budgetID;
+}
+
+async function getTransactions (accessToken, mainBudgetID) {
   var ynab = window.ynab;
-  const accessToken = persAuthToken.data;
   const ynabAPI = new ynab.API(accessToken);
-  
-  (async function() {
-  const transactionResponse = await ynabAPI.transactions.getTransactions(mainBudget.id);
-  const transactions = transactionResponse.data.transactions;
+  const transactionResponse = await ynabAPI.transactions.getTransactions(mainBudgetID);
+  return transactionResponse.data.transactions;
+}
+
+function storeTransactions () {
   for(let transaction of transactions) {
     const amount = parseFloat($(transaction.amount));
     (amount > 0 ? '$2.00' : '$10.00');
     console.log(`Amount: ${transaction.amount}`);
   }
-  })();
 }
