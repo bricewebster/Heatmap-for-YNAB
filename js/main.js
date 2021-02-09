98352df271df13edccc9b2fdd3aa7707f5eb06df
bwebster <erodedpretzel@gmail.com>
2021-02-09 16:53:28 -0500

    Finished storing transactions in expense and income arrays in the users currency decimal format. Limited transactions to hard coded year for now until function is built for user to choose year.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+var expenseTransactions = new Array(366);
+var incomeTransactions = new Array(366);
+var yearChosen = 2020;
+
 main()
 
 async function main() {
   const accessToken = await getAccessToken();
   const mainBudgetID = await getBudgetID();
   const transactions = await getTransactions(accessToken, mainBudgetID);
-  var expenseTransactions = [];
-  var incomeTransactions = [];
-
-  storeTransactions(transactions);
+  const currencyDecimals = await getCurrencyDecimals(accessToken, mainBudgetID);
+  
+  storeTransactions(transactions, currencyDecimals);
+  listTransactions();
 }  
 
 async function getAccessToken() {
@@ -29,19 +33,38 @@ async function getTransactions(accessToken, mainBudgetID) {
   return transactionResponse.data.transactions;
 }
 
-function storeTransactions(transactions) {
+async function getCurrencyDecimals(accessToken, mainBudgetID) {
+  var ynab = window.ynab;
+  const ynabAPI = new ynab.API(accessToken);
+  const budgetResponse = await ynabAPI.budgets.getBudgetSettingsById(mainBudgetID);
+  return budgetResponse.data.settings.currency_format.decimal_digits;
+}
+
+function storeTransactions(transactions, currencyDecimals) {
   for(let transaction of transactions) {
-    const amount = parseFloat($(transaction.amount));
-    const transactionIndex = daysIntoYear(Date.parse($(transactions.Date)));
-    (amount > 0 ? incomeTransactions[transactionIndex] = amount : expenseTransactions[transactionIndex] = amount);
-    //console.log(`Amount: ${transaction.amount}`);
+    const transactionDate = new Date(Date.parse(transaction.date));
+    if(transactionDate.getFullYear() != yearChosen) {
+      continue;
+    } else {
+      const amount = ynab.utils.convertMilliUnitsToCurrencyAmount(transaction.amount, currencyDecimals); //converts to users currency in decimals
+      const transactionIndex = daysIntoYear(transactionDate);
+      if (amount > 0) {
+        (isNaN(incomeTransactions[transactionIndex]) ? incomeTransactions[transactionIndex] = 0 : incomeTransactions[transactionIndex]);
+        incomeTransactions[transactionIndex] += amount;
+      } else { 
+        (isNaN(expenseTransactions[transactionIndex]) ? expenseTransactions[transactionIndex] = 0 : expenseTransactions[transactionIndex]);
+        expenseTransactions[transactionIndex] += amount;
+      }
+    }
   }
 }
 
 function listTransactions (){
-
+  for(incomeIndex = 0; incomeIndex <= incomeTransactions.length; incomeIndex++) {
+    console.log(`Income Day: ${incomeIndex} , Income Amount: ${incomeTransactions[incomeIndex]}`);
+  }
 }
 
 function daysIntoYear(transactionDate){
-  return (Date.UTC(newtransactionDate.getFullYear(), transactionDate.getMonth(), transactionDate.getDate()) - Date.UTC(transactionDate.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+  return (Date.UTC(transactionDate.getFullYear(), transactionDate.getMonth(), transactionDate.getDate()) - Date.UTC(transactionDate.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
 }
