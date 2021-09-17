9b1e41770ac4d21f952a0658594f8fc12c466a9c
bwebster <erodedpretzel@gmail.com>
2021-09-17 16:51:38 -0400

    Added access to YNAB API within App.svelte. Cobbled together so need to clean it all up. Just tested to see if I could get it to work.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,66 @@
 <script>
 	import Navbar from "./components/Navbar.svelte";
 	import Content from "./components/Content.svelte";
+	import { onMount } from 'svelte';
 	
 	let activeTab = 'Yearly';
 
-	console.log(activeTab)
+
+        let ynabAPIReady = false;
+        let mounted = false;
+
+	async function main() {
+		async function getAccessToken() {
+			const response = await fetch('.vscode/accessToken.txt');
+			const accessToken = await response.text();
+			return accessToken;
+		}
+
+		async function getBudgetID() {
+			const response = await fetch('.vscode/budgetID.txt');
+			const budgetID = await response.text();
+			return budgetID;
+		}
+
+		const accessToken = await getAccessToken();
+		const mainBudgetID = await getBudgetID();
+
+	
+	async function getAccounts(accessToken, mainBudgetID) {
+ 		var ynab = window.ynab;
+  		const ynabAPI = new ynab.API(accessToken);
+  		const accountResponse = await ynabAPI.accounts.getAccounts(mainBudgetID);
+  		return accountResponse.data.accounts;
+	}
+	console.log(getAccounts(accessToken,mainBudgetID));
+}
+
+		
+        onMount(() => {
+            // The payment-form is ready.
+            mounted = true;
+            if (ynabAPIReady) {
+                ynabAPIElements();
+            }
+        });
+ 
+        function ynabAPILoaded() {
+            // The external Stripe javascript is ready.
+            ynabAPIReady = true;
+            if (mounted) {
+                ynabAPIElements();
+            }
+        }
+ 
+        function ynabAPIElements() {
+			main();
+        }
 
 </script>
+
+<svelte:head>
+  <script src="https://unpkg.com/ynab@1.21.0/dist/browser/ynab.js" on:load={ynabAPILoaded}></script>
+</svelte:head>
 
 <main>
 	<Navbar bind:activeTab = {activeTab} />
